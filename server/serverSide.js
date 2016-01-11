ab352ca8fe0fc1dead4c63e1f14d252ff10e9912
QuentinS <quentin.sahal@gmail.com>
2016-01-11 01:46:58 +0100

    startup function updated

--- server/serverSide.js
+++ server/serverSide.js
@@ -1,7 +1,16 @@
 points = new Meteor.Collection('pointsCollection');
 
+Meteor.startup(function() {
+if (!points.findOne()) {
+      points.insert(
+      	{w: 1}
+      	);
+    }
+});
+
 Meteor.methods({
   'clear': function () {
     points.remove({});
   }
 });
+
